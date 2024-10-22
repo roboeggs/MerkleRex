e017abbeb7cb710aee12016655fb1a39e0d44382
debian-server <andrey@focusbot.me>
2024-10-22 19:05:21 +0000

    feat: add main menu

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+void menu()
+{   
+    // в примере тип int
+    int userOption;
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats" << std::endl;
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+    // 4 make a bid
+    std::cout << "4: Make a bid " << std::endl;
+    // 5 print wallet
+    std::cout << "5: Print wallet " << std::endl;
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
+
+    std::cout << "============" << std::endl;
+
+    std::cout << "Type in 1-6" << std::endl;
+
+    std::cin >> userOption;
+
+    std::cout << "You chose: " << userOption << std::endl;
+
+    switch (userOption)
+    {
+    case 1:
+        std::cout << "Help - your am is to make money. Analyse the market and make bids and offer" << std::endl;
+        break;
+    case 2:
+        std::cout << "Market looks goog"<< std::endl;
+        break;
+    case 3:
+        std::cout << "Mark and offer - enter the amount" << std::endl;
+        break;
+    case 4:
+        std::cout << "Make a bid - enter the amount" << std::endl;
+        break;
+    case 5:
+        std::cout << "You wallet is empty." << std::endl;
+        break;
+    case 6:
+        std::cout << "Going to next time frame." << std::endl;
+        break;
+
+    default:
+        std::cout << "Invalid choice. Choose 1-6" << std::endl;
+        break;
+    }
+    
+}
+
+int main()
+{
+    while (true)
+    {
+       menu();
+    }
+
+    return 0;
+}
