0620795b4335de510cf919f62d960bccdd0ecba7
Andrei <andfishka@gmail.com>
2024-11-08 12:43:40 +0300

    feat: Write a print menu function and a processOption function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-void menu()
-{   
-    // в примере тип int
-    int userOption;
+void printMenu()
+{
     // 1 print help
     std::cout << "1: Print help " << std::endl;
     // 2 print exchange stats
@@ -17,48 +15,64 @@ void menu()
     // 6 continue
     std::cout << "6: Continue " << std::endl;
 
-
     std::cout << "============" << std::endl;
 
+}
+
+int getUserOption()
+{
     std::cout << "Type in 1-6" << std::endl;
-
+    int userOption;
     std::cin >> userOption;
-
     std::cout << "You chose: " << userOption << std::endl;
+    return userOption;
+}
 
+void processUserOption(int userOption)
+{
     switch (userOption)
-    {
-    case 1:
-        std::cout << "Help - your am is to make money. Analyse the market and make bids and offer" << std::endl;
-        break;
-    case 2:
-        std::cout << "Market looks goog"<< std::endl;
-        break;
-    case 3:
-        std::cout << "Mark and offer - enter the amount" << std::endl;
-        break;
-    case 4:
-        std::cout << "Make a bid - enter the amount" << std::endl;
-        break;
-    case 5:
-        std::cout << "You wallet is empty." << std::endl;
-        break;
-    case 6:
-        std::cout << "Going to next time frame." << std::endl;
-        break;
+        {
+        case 1:
+            std::cout << "Help - your am is to make money. Analyse the market and make bids and offer" << std::endl;
+            break;
+        case 2:
+            std::cout << "Market looks goog"<< std::endl;
+            break;
+        case 3:
+            std::cout << "Mark and offer - enter the amount" << std::endl;
+            break;
+        case 4:
+            std::cout << "Make a bid - enter the amount" << std::endl;
+            break;
+        case 5:
+            std::cout << "You wallet is empty." << std::endl;
+            break;
+        case 6:
+            std::cout << "Going to next time frame." << std::endl;
+            break;
 
-    default:
-        std::cout << "Invalid choice. Choose 1-6" << std::endl;
-        break;
-    }
+        default:
+            std::cout << "Invalid choice. Choose 1-6" << std::endl;
+            break;
+        }
+}
+
+void menu()
+{   
+    // в примере тип int
+    int userOption = getUserOption();
+
+    processUserOption(userOption);
     
 }
 
 int main()
 {
     while (true)
-    {
-       menu();
+    {   
+        printMenu();
+        int userOption = getUserOption();
+        processUserOption(userOption);
     }
 
     return 0;
